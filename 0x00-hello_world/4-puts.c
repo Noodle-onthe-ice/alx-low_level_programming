67fa0577e1caed751d4bbef70487761bed3b9d1d
Noodle-onthe-ice <omarimensahdavid@gmail.com>
2023-02-16 16:27:18 -0800

    1st C code0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * text: char variable storing string
+ * puts: prints content of text
+ * Return: 0 if program run without error, 1 otherwise
+ */
+
 int main(void)
 {
 	char text[] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(text);
-	
-	return 0;
+
+	return (0);
 }
