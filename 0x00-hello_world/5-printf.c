f4e61925f21271581aabc4192b78fcd34f8ded46
Noodle-onthe-ice <omarimensahdavid@gmail.com>
2023-02-16 20:33:26 -0800

    new1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/** main - entry pointof programme
- * printf -prints out to screen
+/** main - entry point of programme
+ * printf - prints out to screen
  * Return: 0 if the programme runs without errors and 1 if otherwise
  */
 
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return(0);}
+	return (0);
+}
