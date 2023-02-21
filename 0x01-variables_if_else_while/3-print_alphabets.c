f45a1a2d1acd5c8d132c04ab83f9bfdafa891a79
Noodle-onthe-ice <omarimensahdavid@gmail.com>
2023-02-20 21:43:09 -0800

    another print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - code entry
+ * Return: 0
+ */
+
+int main(void)
+{
+	char cha;
+
+	for (cha = 'a'; cha <= 'z'; cha++)
+	{
+		putchar(cha);
+	}
+	for (cha = 'A'; cha <= 'Z'; cha++)
+	{
+		putchar(cha);
+		if (char == 'Z')
+		{
+			putchar('\n');
+		}
+	return (0);
+}
