3b5b0bd5c3ffa68c3efb0bd8b9484517954e6db0
Noodle-onthe-ice <omarimensahdavid@gmail.com>
2023-02-22 07:03:20 -0800

    progressing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,14 @@
 
 int main(void)
 {
-	char c[]= '_putchar\n';
-	_putchar(c[0]);
-	_putchar(c[1]);
-	_putchar(c[2]);
-	_putchar(c[3]);
-	_putchar(c[4]);
-	_putchar(c[5]);
-	_putchar(c[6]);
-	_putchar(c[7]);
-	_putchar(c[8]);
-	_putchar(c[9]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
