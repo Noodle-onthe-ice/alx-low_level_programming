2f1913867c9c4b55eca1d1650ee178fccbb49445
Noodle-onthe-ice <omarimensahdavid@gmail.com>
2023-02-23 00:35:23 -0800

    alpha 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ *print_alphabet_x10 - prints alphabets 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(print_alphabet);
+		i += 1;
+	}
+}
