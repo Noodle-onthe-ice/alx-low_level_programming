bbf27dd69804f82f364eee656381bd14acc99bd7
Noodle-onthe-ice <omarimensahdavid@gmail.com>
2023-02-23 00:17:17 -0800

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,12 @@
  * Return: 0
  */
 void print_alphabet(void)
-
-/**
- * main - Entry point
- *
- * Return: 0
- */
-
- int main(void)
 {
-	print_alphabet();
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alphabet);
+	}
+	putchar('\n');
 }
