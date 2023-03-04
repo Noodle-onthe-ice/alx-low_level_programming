bd5a3e4fa52ded4f89b20dc127dd7738a116e016
Noodle-onthe-ice <omarimensahdavid@gmail.com>
2023-03-03 17:16:40 -0800

    cmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,10 @@
 int _strcmp(char *s1, char *s2)
 {
 	int len = u = 0;
-	
+
 	while (len == 0)
 	{
-		if ((*s1 + u) == '\0') && (*(s2 + u) == '\0'))
+		if (((*s1 + u) == '\0') && (*(s2 + u) == '\0'))
 			break;
 		len = *(s1 + u) - *(s2 + u);
 		u++;
