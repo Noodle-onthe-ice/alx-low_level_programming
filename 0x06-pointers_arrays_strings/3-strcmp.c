e890922990850d75de8a83fb6c165a10eacbeaf7
Noodle-onthe-ice <omarimensahdavid@gmail.com>
2023-03-03 16:48:13 -0800

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: 1st parameter
+ * @s2: 2nd parameter
+ * Return: 1, 0 or -1
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 > s2)
+		return (1);
+	else if (s1 == s2)
+		return (0);
+	else
+		return (-1);
+}
